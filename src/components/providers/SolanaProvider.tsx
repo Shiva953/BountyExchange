445db1b126cbb0034d08d1ffb575be29af21f69f
Shiva953 <b22070@students.iitmandi.ac.in>
2026-02-08 11:37:16 +0530

    check build

--- src/components/providers/SolanaProvider.tsx
+++ src/components/providers/SolanaProvider.tsx
@@ -12,10 +12,8 @@ import {
   WalletNotConnectedError,
 } from "@solana/wallet-adapter-base";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import {
-  PhantomWalletAdapter,
-  SolflareWalletAdapter,
-} from "@solana/wallet-adapter-wallets";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
+import { SolflareWalletAdapter } from "@solana/wallet-adapter-solflare";
 
 import "@solana/wallet-adapter-react-ui/styles.css";
 
